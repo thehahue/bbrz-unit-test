e270f3bb07a54e5734030ab06e4610325a87d811
Stefan Hasenhuetl <s.hasenhuetl@mediashop-group.com>
2024-09-27 12:42:07 +0200

    Use wrapper for output.

--- src/main/java/at/bbrz/rechner/RechnerVisualOutput.java
+++ src/main/java/at/bbrz/rechner/RechnerVisualOutput.java
@@ -1,19 +1,22 @@
 package at.bbrz.rechner;
+import at.bbrz.OutputWrapper;
 import at.bbrz.forms.Form;
 import java.util.List;
 
 public class RechnerVisualOutput {
+    private OutputWrapper outputWrapper;
     private final List<Rechner> rechner;
     private final List<Form> formen;
 
-    public RechnerVisualOutput(List<Form> formen) {
+    public RechnerVisualOutput(List<Form> formen, OutputWrapper outputWrapper) {
         rechner = List.of(new UmfangRechner(), new FlaechenRechner());
         this.formen = formen;
+        this.outputWrapper = outputWrapper;
     }
 
     public void printAllSummen() {
         for (Rechner re : rechner) {
-            System.out.println( re.getRechnerName() + "summen: " + re.sum(formen));
+            outputWrapper.printLine( re.getRechnerName() + "summen: " + re.sum(formen));
         }
     }
 }
