9bf584c5bc30e97e316e755fc029c6ec9d7fb914
Stefan Hasenhuetl <s.hasenhuetl@mediashop-group.com>
2024-10-04 12:54:36 +0200

    Add text variables.

--- src/main/java/at/bbrz/rechner/AbstractRechner.java
+++ src/main/java/at/bbrz/rechner/AbstractRechner.java
@@ -5,11 +5,8 @@ import at.bbrz.forms.Form;
 import java.util.List;
 
 public abstract class AbstractRechner implements Rechner {
-    protected String outputMessage;
-
-    public String getOutputMessage(){
-        return outputMessage;
-    }
+    protected String summenText;
+    protected String minText;
 
     protected void checkIfNullOrEmpty(List<Form> forms) {
         if (forms == null || forms.isEmpty()) {
