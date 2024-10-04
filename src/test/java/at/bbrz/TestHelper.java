f661d6caa2c59765fbd8909a7f15921ece7bc0e4
Stefan Hasenhuetl <s.hasenhuetl@mediashop-group.com>
2024-10-04 10:42:13 +0200

    Insert TestHelper class for commonly used methods or classes.

---
+++ src/test/java/at/bbrz/TestHelper.java
@@ -0,0 +1,39 @@
+package at.bbrz;
+
+import at.bbrz.forms.Form;
+
+public class TestHelper {
+
+    public static class TestForm implements Form{
+
+        @Override
+        public double flaeche() {
+            return 1.111;
+        }
+
+        @Override
+        public double umfang() {
+            return 4.55;
+        }
+    }
+    public static class TestFormWithParameters implements Form{
+
+        private double a;
+        private double b;
+
+        public TestFormWithParameters(double a, double b){
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public double flaeche() {
+            return a+b+2*a;
+        }
+
+        @Override
+        public double umfang() {
+            return b+a+a+a;
+        }
+    }
+}
