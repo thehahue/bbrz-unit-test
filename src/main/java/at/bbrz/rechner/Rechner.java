9e188d21fa08b91d412b3ba0120e4560e685922b
Stefan Hasenhuetl <s.hasenhuetl@mediashop-group.com>
2024-10-04 12:54:12 +0200

    Add new message interface methods.

--- src/main/java/at/bbrz/rechner/Rechner.java
+++ src/main/java/at/bbrz/rechner/Rechner.java
@@ -5,7 +5,8 @@ import at.bbrz.forms.Form;
 import java.util.List;
 
 public interface Rechner {
-    String getOutputMessage();
+    String getSummenText();
+    String getMinText();
     double sum(List<Form> forms) throws IllegalArgumentException;
     double max(List<Form> forms) throws IllegalArgumentException;
     double min(List<Form> forms) throws IllegalArgumentException;
