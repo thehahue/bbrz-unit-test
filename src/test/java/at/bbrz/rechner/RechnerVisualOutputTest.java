55fc217b873665de25922e7468147cda1fab8ce1
Stefan Hasenhuetl <s.hasenhuetl@mediashop-group.com>
2024-10-04 09:12:23 +0200

    Refactoring RechnerVisualOutputTest.
    Do not use Kreis and Rechteck implementations in test.

--- src/test/java/at/bbrz/rechner/RechnerVisualOutputTest.java
+++ src/test/java/at/bbrz/rechner/RechnerVisualOutputTest.java
@@ -1,8 +1,7 @@
 package at.bbrz.rechner;
 
 import at.bbrz.OutputWrapper;
-import at.bbrz.forms.Kreis;
-import at.bbrz.forms.Rechteck;
+import at.bbrz.forms.Form;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,25 +13,58 @@ import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 class RechnerVisualOutputTest {
-    
+
     RechnerVisualOutput rechnerVisualOutput;
 
     @Mock
     OutputWrapper outputWrapper;
-    
+
     @BeforeEach
     void setUp() {
         rechnerVisualOutput = new RechnerVisualOutput(List.of(
-                new Kreis(5),
-                new Rechteck(4, 5),
-                new Kreis(4.71)),
+                new TestForm(),
+                new TestFormWithParameters(2.1, 3.05)
+        ),
                 outputWrapper);
     }
 
     @Test
     void printAllSummen() {
         rechnerVisualOutput.printAllSummen();
-        Mockito.verify(outputWrapper).printLine("Umfangsummen: 79.01");
-        Mockito.verify(outputWrapper).printLine("Flächensummen: 168.23");
+        Mockito.verify(outputWrapper).printLine("Umfangsummen: 32.44");
+        Mockito.verify(outputWrapper).printLine("Flächensummen: 26.58");
+    }
+
+    private static class TestForm implements Form {
+
+        @Override
+        public double flaeche() {
+            return 13.13;
+        }
+
+        @Override
+        public double umfang() {
+            return 24.24;
+        }
+    }
+
+    private static class TestFormWithParameters implements Form {
+        private final double a;
+        private final double b;
+
+        public TestFormWithParameters(double a, double b) {
+            this.a = a;
+            this.b = b;
+        }
+
+        @Override
+        public double flaeche() {
+            return a * a * b;
+        }
+
+        @Override
+        public double umfang() {
+            return a + 2 * b;
+        }
     }
 }
